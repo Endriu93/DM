9e7479be432503ca878f30a89890119cd92368d9
Andrzej Węgrzyn <andrzej9393@gmail.com>
2017-09-23 14:38:27 +0200

    configuration OK

---
+++ src/ts/appComponent.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<div>Component_test</div>'
+})
+export class AppComponent {
+}
---
+++ src/ts/module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+// don't know why, but is required
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './appComponent';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  // here we place all used components in appliation
+  declarations: [ AppComponent ],
+  // component, which will bootstrap index.html
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule {
+
+}
