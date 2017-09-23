9412e1274d964b4b2cb2a9c92be7a030173ffac8
Andrzej Węgrzyn <andrzej9393@gmail.com>
2017-09-23 15:46:39 +0200

    clean unused files

--- src/ts/module1.ts
+++
@@ -1,8 +0,0 @@
-export class Animal {
-  public name: string;
-  public constructor(theName: string) { this.name = theName; }
-  public move(distanceInMeters: number) {
-    console.log("");
-  }
-  
-}
--- src/ts/module2.ts
+++
@@ -1,7 +0,0 @@
-import {Animal} from './module1';
-
-var animal = new Animal("lion");
-
-export class Module2{
-  name:string;
-}
