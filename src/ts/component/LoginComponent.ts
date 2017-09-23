0e9ff162a82b506e780e2069811a59fd7bfccc36
Andrzej Węgrzyn <andrzej9393@gmail.com>
2017-09-23 19:06:52 +0200

    LoginComponent

--- src/ts/component/LoginComponent.ts
+++ src/ts/component/LoginComponent.ts
@@ -4,12 +4,26 @@ import {Component, EventEmitter, Output} from '@angular/core';
     templateUrl: 'html/loginComponent.html'
 })
 export class LoginComponent {
-
     onLogged: EventEmitter<boolean> = new EventEmitter<boolean>();
+    username: string;
+    password: string;
 
-    public onUserLogged(): void
-    {
+    public onUserLogged(): void {
         // alert("user Logged!");
         this.onLogged.emit(true);
+    }
+
+    onUsername(event: any) {
+        this.username = event.target.value;
+    }
+
+    onPassword(event: any) {
+        this.password = event.target.value;
+    }
+
+    onSubmit() {
+        //TODO
+        if (this.username == "admin" && this.password == "admin")
+            this.onUserLogged();
     }
 }
